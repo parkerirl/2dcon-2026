1fe318bbb551a7f9ee129fb9c3e865bc54deaa09
Parker Combs <pcombs3@gatech.edu>
2026-08-06 00:44:50 -0500

    Basic test scene

---
+++ test_scene.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+@onready var timer: Timer = $Timer
+@onready var progress_bar: ProgressBar = $ProgressBar
+
+func _ready() -> void:
+	progress_bar.max_value = timer.wait_time
+	timer.start()
+
+func _process(_delta: float) -> void:
+	progress_bar.value = timer.time_left
