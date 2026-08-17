77e64e8dbb2199939a0fafb876e911b82d60283c
Parker Combs <pcombs3@gatech.edu>
2026-08-17 18:27:14 -0500

    laughinggame add basic mechanics

--- stage1/1_no_laughing/1_no_laughing.gd
+++ stage1/1_no_laughing/1_no_laughing.gd
@@ -4,6 +4,12 @@ extends Node2D
 @export var videos: Array[AnimatedSprite2D]
 var videoToShow: AnimatedSprite2D
 
+@export var guy: AnimatedSprite2D
+var guyState: int
+
+var inputDelay: int
+var inputDelayCounter: int
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for sprite in videos:
@@ -11,7 +17,40 @@ func _ready() -> void:
 		
 	videoToShow = videos.pick_random()
 	videoToShow.visible = true
+	
+	guyState = 0
+	guy.play("default")
+	# 0 = not laughing
+	# 1 = laugh_1
+	# 2 = laugh_2
+	# 3 = laugh_3
+	
+	Global.gameWon = true
+	inputDelay = 60
+	inputDelayCounter = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	
+	inputDelayCounter += 1
+	
+	if(inputDelayCounter > inputDelay):
+	
+		# Generate a random int between 1-10 to trigger initial laugh state
+		var random_int = randi_range(1, 100)
+		
+		if(guyState == 0):
+			if(random_int < 20):
+				guy.play("laugh_1")
+				guyState = 1
+				
+		elif(guyState == 1):
+			if(random_int < 10):
+				guy.play("laugh_2")
+				guyState = 2
+				
+		elif(guyState == 2):
+			if(random_int < 10):
+				guy.play("laugh_3")
+				guyState = 3
+				Global.gameWon = false
